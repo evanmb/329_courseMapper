5d31b6816c174dd306eac0477e264c417310ee9a
Christian KIein <cjklein@iastate.edu>
2016-02-25 13:14:44 -0600

    Update Semester.java
    Added main functionality

--- src/main/java/utilities/Semester.java
+++ src/main/java/utilities/Semester.java
@@ -1,5 +1,26 @@
 package utilities;
 
-public class Semester {
+import java.util.ArrayList;
 
+public class Semester {
+	private ArrayList<Course> courses;
+	
+	private int totalCredits;
+	
+	public Semester() {
+		totalCredits = 0;
+	}
+	
+	public void addCourse(Course courseToAdd) {
+		courses.add(courseToAdd);
+		totalCredits += courseToAdd.getCredits();
+	}
+	
+	public ArrayList<Course> getCourses() {
+		return courses;
+	}
+	
+	public int getCredits() {
+		return totalCredits;
+	}
 }
