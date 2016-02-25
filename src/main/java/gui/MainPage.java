7e85db9bf722161337bfa1ab76d862ac8472cf3e
evan.blackwell@mchsi.com <evan.blackwell@mchsi.com>
2016-02-25 02:09:35 -0600

    Initial Project
    I had to make this a Maven Project to use Parse. It looks like it
    should work. Feel free to adjust project structure, just an initial
    skeleton.

---
+++ src/main/java/gui/MainPage.java
@@ -0,0 +1,13 @@
+package gui;
+
+import org.parse4j.Parse;
+
+public class MainPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Parse.initialize("ueGmIfXGhjfg5T31XCDEc0fkT5uEVZVBfIDGLxBp", 
+				"XXZyCO5GmBUAAm9mirRaxfW91WqmxWf3cn4HN3iu");
+	}
+
+}
