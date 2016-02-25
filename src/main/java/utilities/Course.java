fd73743c007461fc34b1bd57fa377110df6fe997
Christian KIein <cjklein@iastate.edu>
2016-02-25 13:29:16 -0600

    Update Course.java
    Added main functionality

--- src/main/java/utilities/Course.java
+++ src/main/java/utilities/Course.java
@@ -1,5 +1,29 @@
 package utilities;
 
-public class Course {
+import java.util.ArrayList;
 
+public class Course {
+	private int credits;
+	
+	private String courseCode;
+	
+	private ArrayList<Course> preReqs;
+	
+	public Course(String courseCode) {
+		preReqs = new ArrayList<Course>();
+		
+		//PARSE MUMBO JUMBO
+	}
+	
+	public int getCredits() {
+		return credits;
+	}
+	
+	public String getCourseCode() {
+		return courseCode;
+	}
+	
+	public ArrayList<Course> getPreReqs() {
+		return preReqs;
+	}
 }
