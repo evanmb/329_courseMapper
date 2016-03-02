8fbae50b7acb68b8bd6e7ba3e81fb596436358d5
evan.blackwell@mchsi.com <evan.blackwell@mchsi.com>
2016-03-02 14:54:20 -0600

    Major Implementation

--- src/main/java/utilities/Major.java
+++ src/main/java/utilities/Major.java
@@ -2,13 +2,37 @@ package utilities;
 
 import java.util.ArrayList;
 
+import org.parse4j.ParseException;
 import org.parse4j.ParseObject;
+import org.parse4j.ParseQuery;
 
 public class Major {
 	private String objectID;
+	private ArrayList<String> coreClasses;
+	private String name;
+	private String code;
 	
 	public Major(ParseObject object) {
-		objectID = object.getObjectId();
+		fillFromParseObject(object);
+	}
+	
+	public Major(String objectID) {
+		ParseQuery<ParseObject> findMajor = new ParseQuery<ParseObject>("Majors");
+		try {
+			fillFromParseObject(findMajor.get(objectID));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void fillFromParseObject(ParseObject parseMajor) {
+		objectID = parseMajor.getObjectId();
+		name = parseMajor.getString("name");
+		code = parseMajor.getString("majorCode");
+
+		coreClasses = new ArrayList<String>();
+		for (Object o : parseMajor.getList("coreClasses"))
+			coreClasses.add((String) o);
 	}
 	
 	public ArrayList<Semester> makeSchedule(int maxCredits, int minCredits) {
