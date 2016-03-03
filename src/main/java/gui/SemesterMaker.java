83048dc9aff069ab7f537d08142b7a4fbc34721a
KitGithub <krkohl@iastate.edu>
2016-03-03 00:47:19 -0600

    SwingGuiParts
    I was still having trouble with parse so that the main page needs to be updated to handle parse. I tested it with the course and semester objects and it appears to work alright.

---
+++ src/main/java/gui/SemesterMaker.java
@@ -0,0 +1,89 @@
+package gui;
+
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.util.ArrayList;
+
+import javax.swing.JPanel;
+import javax.swing.JTextPane;
+
+import utilities.Course;
+import utilities.Semester;
+
+public class SemesterMaker {
+
+		private JPanel panel;
+		private GridBagConstraints gridCon;
+		private Semester semester;
+		private int semesterNum;
+		
+		public SemesterMaker(Semester courses, int numberOfSemester)
+		{
+			this.semester = courses;
+			semesterNum = numberOfSemester;
+			makeSemester();
+			
+		}
+		
+		
+		private void makeSemester()
+		{
+			panel = new JPanel();
+			gridCon = new GridBagConstraints();
+			gridCon.insets = new Insets(0, 0, 5, 0);
+			gridCon.fill = GridBagConstraints.BOTH;
+			gridCon.gridx = 0;
+			gridCon.gridy = semesterNum;
+			//contentPane.add(panel_1, gbc_panel_1);
+			panel.setLayout(new GridLayout(1, 0, 10, 10));
+			//panel.setBackground(Color.WHITE);
+
+			for(int i = 0; i < semester.getCourses().size(); i++)
+			{
+				panel.add(makeCourse(semester.getCourses().get(i)));
+				
+			}
+			panel.add(lastPanel(semester.getCredits()));
+		}
+		
+		private JTextPane makeCourse(Course course)
+		{
+			JTextPane txtpnTest = new JTextPane();
+			txtpnTest.setText(course.getCourseCode());
+			txtpnTest.setEditable(false);
+			//contentPane.add(txtpnTest);
+			if(semesterNum%2 == 0){
+				txtpnTest.setBackground(Color.WHITE);
+			}else
+			{
+				txtpnTest.setBackground(Color.YELLOW);
+			}
+			return txtpnTest;
+		}
+		
+		private JTextPane lastPanel(int credits)
+		{
+			JTextPane txtpnTest = new JTextPane();
+			txtpnTest.setText("credits " + credits + "\nSemester " + (semesterNum+1));
+			txtpnTest.setEditable(false);
+			if(semesterNum%2 == 0){
+				txtpnTest.setBackground(Color.WHITE);
+			}else
+			{
+				txtpnTest.setBackground(Color.YELLOW);
+			}
+			return txtpnTest;
+		}
+		
+		public JPanel getPanel()
+		{
+			return panel;
+		}
+		
+		public GridBagConstraints getGridCon()
+		{
+			return gridCon;
+		}
+}
